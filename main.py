85707bc62822f2428e24596410bfad576374ac72
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2023-11-15 21:45:54 +0100

    Add: isAdmin endpoint

--- main.py
+++ main.py
@@ -27,3 +27,16 @@ def main():
 
     return results
 
+@app.route("/isAdmin")
+def isAdmin():
+    user_mail = request.args.get('mail')
+    database = spanner_client.instance(instance_id).database(database_id)
+    with database.snapshot() as snapshot:
+        cursor = snapshot.execute_sql(
+            "SELECT is_admin FROM user WHERE mail = @userMail;",
+            params={"userMail": user_mail},
+            param_types={"userMail": spanner.param_types.STRING})
+
+    results_list=  list(cursor)
+    is_admin = results_list[0][0] if results_list else False
+    return jsonify(is_admin=is_admin)
